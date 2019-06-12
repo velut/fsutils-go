b62402af9bad351784114b01c754997e08effaff
Edoardo Scibona <velut@users.noreply.github.com>
2019-06-12 19:40:51 +0200

    add file utils

---
+++ fs/file.go
@@ -0,0 +1,229 @@
+package fs
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+const defaultFilePermissions = 0644
+
+// FileInfo represents the information available on a regular file.
+type FileInfo struct {
+	Name string // base name of the file
+	Ext  string // file extension
+	Dir  string // directory containing the file
+	Path string // full file path
+	Size int64  // file size in bytes
+}
+
+// MoveFileSafe moves the file with the given filename to the given destination.
+// If the given destination already exists, MoveFileSafe prevents overwrites by
+// trying other destinations in incrementing order, up to maxTries times.
+// MoveFileSafe returns the destination to which the file is moved.
+func MoveFileSafe(filename, destFilename string, maxTries int) (string, error) {
+	destFilename, err := CopyFileSafe(filename, destFilename, maxTries)
+	if err != nil {
+		return "", err
+	}
+	_ = RemoveFile(filename)
+	return destFilename, nil
+}
+
+// CopyFileSafe copies the file with the given filename to the given destination.
+// If the given destination already exists, CopyFileSafe prevents overwrites by
+// trying other destinations in incrementing order, up to maxTries times.
+// CopyFileSafe returns the destination to which the file is copied.
+func CopyFileSafe(filename, destFilename string, maxTries int) (string, error) {
+	if err := assertCopyable(filename, destFilename); err != nil {
+		return "", err
+	}
+
+	destFile, err := CreateNextFile(destFilename, maxTries)
+	if err != nil {
+		return "", err
+	}
+	if err := destFile.Close(); err != nil {
+		return "", err
+	}
+
+	destFilename = destFile.Name()
+	if err := copyFile(filename, destFilename); err != nil {
+		return "", err
+	}
+
+	return destFilename, nil
+}
+
+// CreateNextFile creates a file based on the given filename and returns it.
+// If filename already exists, CreateNextFile inserts a counter in the filename
+// and tries to create that file. The counter goes from 1 to maxTries included.
+func CreateNextFile(filename string, maxTries int) (*os.File, error) {
+	dir, name := filepath.Split(filepath.Clean(filename))
+
+	for i := 0; i <= maxTries; i++ {
+		filename = filepath.Join(dir, insertCounter(name, i))
+		file, err := CreateFile(filename)
+		if err != nil {
+			continue
+		}
+		return file, nil
+	}
+
+	return nil, errors.New("exceeded maximum number of tries")
+}
+
+// insertCounter inserts a counter in the given filename with the given value.
+// The counter is inserted before the first dot, if any.
+// For example, given filename "test.json" and a value of 3,
+// insertCounter returns "test(3).json".
+// If the value is less than 1, the original filename is returned.
+func insertCounter(filename string, value int) string {
+	if value < 1 {
+		return filename
+	}
+
+	insertPos := strings.Index(filename, ".")
+	if insertPos == -1 {
+		insertPos = len(filename)
+	}
+
+	counter := fmt.Sprintf("(%d)", value)
+	newFilename := filename[:insertPos] + counter + filename[insertPos:]
+	return newFilename
+}
+
+// CreateFile creates a file with the given filename and returns it.
+// If filename already exists, CreateFile returns an error.
+// CreateFile requires exclusive access to the given filename.
+func CreateFile(filename string) (*os.File, error) {
+	// Exclusive access to filename,
+	// see https://golang.org/src/os/error_test.go
+	// and https://stackoverflow.com/a/22483001
+	file, err := os.OpenFile(
+		filename,
+		os.O_RDWR|os.O_CREATE|os.O_EXCL,
+		defaultFilePermissions,
+	)
+	if err != nil {
+		return nil, err
+	}
+	return file, nil
+}
+
+// MoveFile moves the file with the given filename to the given destination.
+// MoveFile overwrites existing destination files.
+func MoveFile(filename, destFilename string) error {
+	if err := CopyFile(filename, destFilename); err != nil {
+		return err
+	}
+	_ = RemoveFile(filename)
+	return nil
+}
+
+// CopyFile copies the file with the given filename to the given destination.
+// CopyFile overwrites existing destination files.
+func CopyFile(filename, destFilename string) error {
+	if err := assertCopyable(filename, destFilename); err != nil {
+		return err
+	}
+
+	return copyFile(filename, destFilename)
+}
+
+func assertCopyable(filename, destFilename string) error {
+	srcInfo, err := os.Stat(filename)
+	if err != nil {
+		return err
+	}
+	if !srcInfo.Mode().IsRegular() {
+		return fmt.Errorf("%q is not a regular file", filename)
+	}
+
+	if strings.TrimSpace(destFilename) == "" {
+		return fmt.Errorf("destination name cannot be empty")
+	}
+
+	destInfo, _ := os.Stat(destFilename)
+	destExistsAndNotRegular := destInfo != nil && !destInfo.Mode().IsRegular()
+	if destExistsAndNotRegular {
+		return fmt.Errorf("%q is not a regular file", destFilename)
+	}
+
+	sameFile := destInfo != nil && os.SameFile(srcInfo, destInfo)
+	if sameFile {
+		return errors.New("cannot copy to the file itself")
+	}
+
+	return nil
+}
+
+func copyFile(filename, destFilename string) error {
+	srcFile, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	defer srcFile.Close()
+
+	destFile, err := os.Create(destFilename)
+	if err != nil {
+		return err
+	}
+
+	if _, err := io.Copy(destFile, srcFile); err != nil {
+		if err1 := destFile.Close(); err1 != nil {
+			return err1
+		}
+		return err
+	}
+
+	return destFile.Close()
+}
+
+// RemoveFile removes the file with the given filename.
+func RemoveFile(filename string) error {
+	return os.Remove(filename)
+}
+
+// ReadFileInfo returns the information available on a regular file.
+func ReadFileInfo(filename string) (*FileInfo, error) {
+	info, err := os.Stat(filename)
+	if err != nil {
+		return nil, err
+	}
+	if !info.Mode().IsRegular() {
+		return nil, fmt.Errorf("%q is not a regular file", filename)
+	}
+
+	name := info.Name()
+	path := filepath.Clean(filename)
+	fi := &FileInfo{
+		Name: name,
+		Ext:  filepath.Ext(name),
+		Dir:  filepath.Dir(path),
+		Path: path,
+		Size: info.Size(),
+	}
+	return fi, nil
+}
+
+// AssertFile returns an error if the given filename is not a regular file.
+func AssertFile(filename string) error {
+	isRegular, err := IsFile(filename)
+	if err != nil || !isRegular {
+		return fmt.Errorf("%q is not  regular file", filename)
+	}
+	return nil
+}
+
+// IsFile returns true if the given filename represents a regular file.
+func IsFile(filename string) (bool, error) {
+	info, err := os.Stat(filename)
+	if err != nil {
+		return false, err
+	}
+	return info.Mode().IsRegular(), nil
+}
---
+++ fs/file_test.go
@@ -0,0 +1,836 @@
+package fs
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIsFile(t *testing.T) {
+	assert := assert.New(t)
+
+	dir1, err := ioutil.TempDir("", "dir")
+	assert.Nil(err)
+	defer os.RemoveAll(dir1)
+
+	file1, err := ioutil.TempFile("", "file")
+	assert.Nil(err)
+	file1.Close()
+	defer os.Remove(file1.Name())
+
+	type args struct {
+		filename string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    bool
+		wantErr bool
+	}{
+		{
+			"non-existing file",
+			args{
+				"",
+			},
+			false,
+			true,
+		},
+		{
+			"directory is not a regular file",
+			args{
+				dir1,
+			},
+			false,
+			false,
+		},
+		{
+			"regular file",
+			args{
+				file1.Name(),
+			},
+			true,
+			false,
+		},
+	}
+	for _, tt := range tests {
+		got, gotErr := IsFile(tt.args.filename)
+		assert.Equal(tt.wantErr, gotErr != nil, tt.name)
+		assert.Equal(tt.want, got, tt.name)
+	}
+}
+
+func TestAssertFile(t *testing.T) {
+	assert := assert.New(t)
+
+	dir1, err := ioutil.TempDir("", "dir")
+	assert.Nil(err)
+	defer os.RemoveAll(dir1)
+
+	file1, err := ioutil.TempFile("", "file")
+	assert.Nil(err)
+	file1.Close()
+	defer os.Remove(file1.Name())
+
+	type args struct {
+		filename string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			"non-existing file",
+			args{
+				"",
+			},
+			true,
+		},
+		{
+			"directory is not a regular file",
+			args{
+				dir1,
+			},
+			true,
+		},
+		{
+			"regular file",
+			args{
+				file1.Name(),
+			},
+			false,
+		},
+	}
+	for _, tt := range tests {
+		gotErr := AssertFile(tt.args.filename)
+		assert.Equal(tt.wantErr, gotErr != nil, tt.name)
+	}
+}
+
+func TestReadFileInfo(t *testing.T) {
+	assert := assert.New(t)
+
+	dir1, err := ioutil.TempDir("", "dir")
+	assert.Nil(err)
+	defer os.RemoveAll(dir1)
+
+	file1, err := ioutil.TempFile("", "file*.txt")
+	assert.Nil(err)
+	file1.Close()
+	defer os.Remove(file1.Name())
+
+	type args struct {
+		filename string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *FileInfo
+		wantErr bool
+	}{
+		{
+			"non-existing file",
+			args{
+				"",
+			},
+			nil,
+			true,
+		},
+		{
+			"directory is not a regular file",
+			args{
+				dir1,
+			},
+			nil,
+			true,
+		},
+		{
+			"regular file",
+			args{
+				file1.Name(),
+			},
+			&FileInfo{
+				Name: filepath.Base(file1.Name()),
+				Ext:  filepath.Ext(file1.Name()),
+				Dir:  filepath.Dir(file1.Name()),
+				Path: file1.Name(),
+				Size: 0,
+			},
+			false,
+		},
+	}
+	for _, tt := range tests {
+		got, gotErr := ReadFileInfo(tt.args.filename)
+		assert.Equal(tt.wantErr, gotErr != nil, tt.name)
+		assert.Equal(tt.want, got, tt.name)
+	}
+}
+
+func TestCreateFile(t *testing.T) {
+	assert := assert.New(t)
+
+	dir1, err := ioutil.TempDir("", "dir")
+	assert.Nil(err)
+	defer os.RemoveAll(dir1)
+
+	file1, err := ioutil.TempFile("", "file*.txt")
+	assert.Nil(err)
+	file1.Close()
+	defer os.Remove(file1.Name())
+
+	type args struct {
+		filename string
+	}
+	tests := []struct {
+		name     string
+		args     args
+		wantFile bool
+		wantErr  bool
+	}{
+		{
+			"directory already exists",
+			args{
+				dir1,
+			},
+			false,
+			true,
+		},
+		{
+			"file already exists",
+			args{
+				file1.Name(),
+			},
+			false,
+			true,
+		},
+		{
+			"filename is available",
+			args{
+				filepath.Join(dir1, "file"),
+			},
+			true,
+			false,
+		},
+	}
+	for _, tt := range tests {
+		got, gotErr := CreateFile(tt.args.filename)
+		assert.Equal(tt.wantErr, gotErr != nil, tt.name)
+		assert.Equal(tt.wantFile, got != nil, tt.name)
+
+		if gotErr != nil {
+			got.Close()
+		}
+	}
+}
+
+func TestRemoveFile(t *testing.T) {
+	assert := assert.New(t)
+
+	dir1, err := ioutil.TempDir("", "dir")
+	assert.Nil(err)
+	file1, err := ioutil.TempFile(dir1, "file*.txt")
+	assert.Nil(err)
+	file1.Close()
+	defer os.RemoveAll(dir1)
+
+	file2, err := ioutil.TempFile("", "file*.txt")
+	assert.Nil(err)
+	file2.Close()
+	defer os.Remove(file2.Name())
+
+	type args struct {
+		filename string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			"invalid file",
+			args{
+				"",
+			},
+			true,
+		},
+		{
+			"non-empty directory",
+			args{
+				dir1,
+			},
+			true,
+		},
+		{
+			"removable file",
+			args{
+				file2.Name(),
+			},
+			false,
+		},
+	}
+	for _, tt := range tests {
+		gotErr := RemoveFile(tt.args.filename)
+		assert.Equal(tt.wantErr, gotErr != nil, tt.name)
+	}
+}
+
+func TestCreateNextFile(t *testing.T) {
+	assert := assert.New(t)
+
+	dir1, err := ioutil.TempDir("", "dir")
+	assert.Nil(err)
+	file1, err := ioutil.TempFile(dir1, "file*.txt")
+	assert.Nil(err)
+	file1.Close()
+	file2, err := ioutil.TempFile(dir1, "file*")
+	assert.Nil(err)
+	file2.Close()
+	defer os.RemoveAll(dir1)
+
+	type args struct {
+		filename string
+		maxTries int
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    string
+		wantErr bool
+	}{
+		{
+			"existing file with extension, zero tries",
+			args{
+				file1.Name(),
+				0,
+			},
+			"",
+			true,
+		},
+		{
+			"existing file with extension, one try",
+			args{
+				file1.Name(),
+				1,
+			},
+			strings.TrimSuffix(file1.Name(), ".txt") + "(1)" + ".txt",
+			false,
+		},
+		{
+			"existing file without extension, zero tries",
+			args{
+				file2.Name(),
+				0,
+			},
+			"",
+			true,
+		},
+		{
+			"existing file without extension, one try",
+			args{
+				file2.Name(),
+				1,
+			},
+			file2.Name() + "(1)",
+			false,
+		},
+	}
+	for _, tt := range tests {
+		got, gotErr := CreateNextFile(tt.args.filename, tt.args.maxTries)
+		assert.Equal(tt.wantErr, gotErr != nil, tt.name)
+		if got != nil {
+			assert.Equal(tt.want, got.Name(), tt.name)
+		}
+	}
+}
+
+func TestCopyFile(t *testing.T) {
+	assert := assert.New(t)
+
+	dir1, err := ioutil.TempDir("", "dir")
+	assert.Nil(err)
+	file1Name := filepath.Join(dir1, "file1.txt")
+	err = ioutil.WriteFile(
+		file1Name,
+		[]byte("hello world"),
+		defaultFilePermissions,
+	)
+	assert.Nil(err)
+	file2, err := ioutil.TempFile(dir1, "file")
+	assert.Nil(err)
+	file2.Close()
+	file3, err := ioutil.TempFile(dir1, "file")
+	assert.Nil(err)
+	defer file3.Close()
+	file4, err := CreateFile(filepath.Join(dir1, "file4"))
+	assert.Nil(err)
+	defer file4.Close()
+	defer os.RemoveAll(dir1)
+
+	type args struct {
+		filename     string
+		destFilename string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			"invalid filename",
+			args{
+				"",
+				file2.Name(),
+			},
+			true,
+		},
+		{
+			"invalid copy from directory",
+			args{
+				dir1,
+				file2.Name(),
+			},
+			true,
+		},
+		{
+			"invalid copy to directory",
+			args{
+				file1Name,
+				dir1,
+			},
+			true,
+		},
+		{
+			"invalid copy to same file",
+			args{
+				file1Name,
+				file1Name,
+			},
+			true,
+		},
+		{
+			"invalid copy to invalid file",
+			args{
+				file1Name,
+				"",
+			},
+			true,
+		},
+		{
+			"copy to another file",
+			args{
+				file1Name,
+				file2.Name(),
+			},
+			false,
+		},
+		{
+			"copy to open file",
+			args{
+				file1Name,
+				file3.Name(),
+			},
+			false,
+		},
+		{
+			"copy to exclusive file",
+			args{
+				file1Name,
+				file4.Name(),
+			},
+			false,
+		},
+	}
+	for _, tt := range tests {
+		gotErr := CopyFile(tt.args.filename, tt.args.destFilename)
+		assert.Equal(tt.wantErr, gotErr != nil, tt.name)
+	}
+}
+
+func TestMoveFile(t *testing.T) {
+	assert := assert.New(t)
+
+	dir1, err := ioutil.TempDir("", "dir")
+	assert.Nil(err)
+	file1Name := filepath.Join(dir1, "file1.txt")
+	err = ioutil.WriteFile(
+		file1Name,
+		[]byte("hello world"),
+		defaultFilePermissions,
+	)
+	assert.Nil(err)
+	file2, err := ioutil.TempFile(dir1, "file")
+	assert.Nil(err)
+	file2.Close()
+	file3, err := ioutil.TempFile(dir1, "file")
+	assert.Nil(err)
+	defer file3.Close()
+	file4, err := CreateFile(filepath.Join(dir1, "file4"))
+	assert.Nil(err)
+	defer file4.Close()
+	defer os.RemoveAll(dir1)
+
+	type args struct {
+		filename     string
+		destFilename string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			"invalid filename",
+			args{
+				"",
+				file2.Name(),
+			},
+			true,
+		},
+		{
+			"invalid move from directory",
+			args{
+				dir1,
+				file2.Name(),
+			},
+			true,
+		},
+		{
+			"invalid move to directory",
+			args{
+				file1Name,
+				dir1,
+			},
+			true,
+		},
+		{
+			"invalid move to same file",
+			args{
+				file1Name,
+				file1Name,
+			},
+			true,
+		},
+		{
+			"invalid move to invalid file",
+			args{
+				file1Name,
+				"",
+			},
+			true,
+		},
+		{
+			"move to another file",
+			args{
+				file1Name,
+				file2.Name(),
+			},
+			false,
+		},
+		{
+			"move to open file",
+			args{
+				file2.Name(),
+				file3.Name(),
+			},
+			false,
+		},
+		{
+			"move to exclusive file",
+			args{
+				file3.Name(),
+				file4.Name(),
+			},
+			false,
+		},
+	}
+	for _, tt := range tests {
+		gotErr := MoveFile(tt.args.filename, tt.args.destFilename)
+		if tt.wantErr != (gotErr != nil) {
+			fmt.Println()
+			fmt.Println(tt.args.filename)
+			fmt.Println(tt.args.destFilename)
+			fmt.Println(gotErr)
+			fmt.Println()
+		}
+		assert.Equal(tt.wantErr, gotErr != nil, tt.name)
+	}
+}
+
+func TestCopyFileSafe(t *testing.T) {
+	assert := assert.New(t)
+
+	dir1, err := ioutil.TempDir("", "dir")
+	assert.Nil(err)
+	file1Name := filepath.Join(dir1, "file1.txt")
+	err = ioutil.WriteFile(
+		file1Name,
+		[]byte("hello world"),
+		defaultFilePermissions,
+	)
+	assert.Nil(err)
+	file2, err := ioutil.TempFile(dir1, "file")
+	assert.Nil(err)
+	file2.Close()
+	file3, err := ioutil.TempFile(dir1, "file")
+	assert.Nil(err)
+	defer file3.Close()
+	file4, err := CreateFile(filepath.Join(dir1, "file4"))
+	assert.Nil(err)
+	defer file4.Close()
+	defer os.RemoveAll(dir1)
+
+	type args struct {
+		filename     string
+		destFilename string
+		maxTries     int
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    string
+		wantErr bool
+	}{
+		{
+			"invalid filename",
+			args{
+				"",
+				file1Name,
+				1,
+			},
+			"",
+			true,
+		},
+		{
+			"invalid copy from directory",
+			args{
+				dir1,
+				file2.Name(),
+				1,
+			},
+			"",
+			true,
+		},
+		{
+			"invalid copy to dir",
+			args{
+				file1Name,
+				dir1,
+				1,
+			},
+			"",
+			true,
+		},
+		{
+			"invalid copy to same file",
+			args{
+				file1Name,
+				file1Name,
+				1,
+			},
+			"",
+			true,
+		},
+		{
+			"invalid copy to invalid file",
+			args{
+				file1Name,
+				"",
+				1,
+			},
+			"",
+			true,
+		},
+		{
+			"existing destination, zero maxTries",
+			args{
+				file1Name,
+				file2.Name(),
+				0,
+			},
+			"",
+			true,
+		},
+		{
+			"existing destination, one maxTries",
+			args{
+				file1Name,
+				file2.Name(),
+				1,
+			},
+			file2.Name() + "(1)",
+			false,
+		},
+		{
+			"copy to open file",
+			args{
+				file1Name,
+				file3.Name(),
+				1,
+			},
+			file3.Name() + "(1)",
+			false,
+		},
+		{
+			"copy to exclusive file",
+			args{
+				file1Name,
+				file4.Name(),
+				1,
+			},
+			file4.Name() + "(1)",
+			false,
+		},
+	}
+	for _, tt := range tests {
+		got, gotErr := CopyFileSafe(tt.args.filename, tt.args.destFilename, tt.args.maxTries)
+		assert.Equal(tt.wantErr, gotErr != nil, tt.name)
+		assert.Equal(tt.want, got, tt.name)
+	}
+}
+
+func TestMoveFileSafe(t *testing.T) {
+	assert := assert.New(t)
+
+	dir1, err := ioutil.TempDir("", "dir")
+	assert.Nil(err)
+	file1Name := filepath.Join(dir1, "file1.txt")
+	err = ioutil.WriteFile(
+		file1Name,
+		[]byte("hello world"),
+		defaultFilePermissions,
+	)
+	assert.Nil(err)
+	file2, err := ioutil.TempFile(dir1, "file")
+	assert.Nil(err)
+	file2.Close()
+	file3, err := ioutil.TempFile(dir1, "file")
+	assert.Nil(err)
+	defer file3.Close()
+	file4, err := CreateFile(filepath.Join(dir1, "file4"))
+	assert.Nil(err)
+	defer file4.Close()
+	defer os.RemoveAll(dir1)
+
+	type args struct {
+		filename     string
+		destFilename string
+		maxTries     int
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    string
+		wantErr bool
+	}{
+		{
+			"invalid filename",
+			args{
+				"",
+				file1Name,
+				1,
+			},
+			"",
+			true,
+		},
+		{
+			"invalid move from directory",
+			args{
+				dir1,
+				file2.Name(),
+				1,
+			},
+			"",
+			true,
+		},
+		{
+			"invalid move to dir",
+			args{
+				file1Name,
+				dir1,
+				1,
+			},
+			"",
+			true,
+		},
+		{
+			"invalid move to same file",
+			args{
+				file1Name,
+				file1Name,
+				1,
+			},
+			"",
+			true,
+		},
+		{
+			"invalid move to invalid file (1)",
+			args{
+				file1Name,
+				"",
+				1,
+			},
+			"",
+			true,
+		},
+		{
+			"invalid move to invalid file (2)",
+			args{
+				file1Name,
+				"   ",
+				1,
+			},
+			"",
+			true,
+		},
+		{
+			"existing destination, zero maxTries",
+			args{
+				file1Name,
+				file2.Name(),
+				0,
+			},
+			"",
+			true,
+		},
+		{
+			"existing destination, one maxTries",
+			args{
+				file1Name,
+				file2.Name(),
+				1,
+			},
+			file2.Name() + "(1)",
+			false,
+		},
+		{
+			"move to open file",
+			args{
+				file2.Name(),
+				file3.Name(),
+				1,
+			},
+			file3.Name() + "(1)",
+			false,
+		},
+		{
+			"move to exclusive file",
+			args{
+				file3.Name(),
+				file4.Name(),
+				1,
+			},
+			file4.Name() + "(1)",
+			false,
+		},
+	}
+	for _, tt := range tests {
+		got, gotErr := MoveFileSafe(tt.args.filename, tt.args.destFilename, tt.args.maxTries)
+		assert.Equal(tt.wantErr, gotErr != nil, tt.name)
+		assert.Equal(tt.want, got, tt.name)
+	}
+}
